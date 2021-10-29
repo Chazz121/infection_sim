82ef786edcd9287fa272083aae64c206842376c4
Charles Mitchell <chrlsmitch123@gmail.com>
2021-10-29 02:43:44 -0400

    initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[derive(Debug)]
 struct Position {
   x: usize,
@@ -5,53 +7,144 @@ struct Position {
 }
 
 #[derive(Debug)]
-pub struct Person {
+struct Boundry {
+  height: usize,
+  width: usize,
+}
+
+#[derive(Debug)]
+struct Person {
   position: Position,
   infected: bool,
 }
 
 #[derive(Debug)]
 pub struct World {
-  map: Vec<Vec<Option<Person>>>
+  people: Vec<Person>,
+  boundry: Boundry,
+}
+impl std::cmp::PartialEq for Position{
+  fn eq(&self, other: &Position) -> bool{
+    self.x == other.x && self.y == other.y
+  }
+}
+
+impl Clone for Position{
+  fn clone(&self) -> Self {
+    Position{
+      x: self.x,
+      y: self.y
+    }
+  }
 }
 
 impl Person {
-  pub fn from(x_pos: usize, y_pos: usize) -> Person {
+  pub fn from(x_pos: usize, y_pos: usize, boundry: &Boundry, infected: bool) -> Person {
     Person {
       position: Position { x: x_pos, y: y_pos },
-      infected: false,
+      infected,
     }
   }
 
+  pub fn random(boundry: &Boundry, infected: bool) -> Person {
+    use rand::Rng;
+    let mut rng = rand::thread_rng();
+
+    let y_pos = rng.gen_range(0..=boundry.height);
+    let x_pos = rng.gen_range(0..=boundry.width);
+
+    let position = Position { x: x_pos, y: y_pos };
+
+    Person {
+      infected,
+      position: position,
+    }
+  }
+
+  pub fn walk(&mut self, boundry: &Boundry) {
+    use rand::Rng;
+    let mut rng = rand::thread_rng();
+    if boundry.width == 0 {
+      ();
+    } else if self.position.x == 0 {
+      self.position.x = self.position.x + rng.gen_range(0..=1)
+    } else if self.position.x == boundry.width {
+      self.position.x = self.position.x - rng.gen_range(0..=1)
+    } else {
+      let operation = rng.gen_range(0..1);
+      if operation == 0 {
+        self.position.x = self.position.x + rng.gen_range(0..=1)
+      } else {
+        self.position.x = self.position.x - rng.gen_range(0..=1)
+      }
+    }
+
+    if boundry.height == 0 {
+      ();
+    } else if self.position.y == 0 {
+      self.position.y = self.position.y + rng.gen_range(0..=1)
+    } else if self.position.y == boundry.width {
+      self.position.y = self.position.y - rng.gen_range(0..=1)
+    } else {
+      let operation = rng.gen_range(0..1);
+      if operation == 0 {
+        self.position.y = self.position.y + rng.gen_range(0..=1)
+      } else {
+        self.position.y = self.position.y - rng.gen_range(0..=1)
+      }
+    }
+  }
+
+  pub fn infect(&mut self) {
+    self.infected = true
+  }
 }
 
-impl World{
-  fn create(rows: usize, cols:usize) -> World{
-    let columns = vec![Option::None; cols];
-    let rows = vec![columns; rows];
-    World {
-      map: rows
+impl World {
+
+  pub fn create(
+    height: usize,
+    width: usize,
+    population: usize,
+    mut num_of_infected: usize,
+  ) -> World {
+    let boundry = Boundry { height, width };
+
+    let mut people: Vec<Person> = Vec::new();
+
+    for _ in 0..population {
+      if num_of_infected > 0 {
+        let person = Person::random(&boundry, true);
+        people.push(person);
+        num_of_infected -= 1;
+      } else {
+        let person = Person::random(&boundry, false);
+        people.push(person)
+      }
+    }
+
+    World { people, boundry }
+  }
+
+  pub fn move_people(&mut self) {
+    for person in &mut self.people {
+      person.walk(&self.boundry)
     }
   }
 
-  fn add_person(&mut self, person: Person) {
-    let row = 0;
-    let col = 0;
+  pub fn infect_people(&mut self) {
+    let mut infection_list: Vec<Position> = Vec::new();
 
-
-    let mut spot = self.map[row][col];
-
-    spot = Option::Some(person)
-  }
-
-  pub fn from(rows: usize, cols: usize, population: usize) -> World{
-    let mut world = World::create(rows, cols);
-
-    for i in 0..population {
-      let person = Person::from(0,0);
-      world.add_person(person);
+    for person in &mut self.people {
+      if person.infected {
+        infection_list.push(person.position.clone())
+      }
     }
 
-    world
+    for person in &mut self.people {
+      if infection_list.contains(&mut person.position) {
+          person.infect()
+      }
+    }
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use infection_sim::Person;
 use infection_sim::World;
 
 fn main() {
-  let person = Person::from(35,2);
 
-  println!("person: {:#?}", person);
+  let mut world = World::create(1, 1, 2, 1);
+  world.infect_people();
+  println!("{:?}", &world);
 
-  let world = World::from(1, 1, 1);
-
-  println!("{:#?}", world);
+  world.move_people();
+  world.infect_people();
+  println!("{:?}", &world);
+  world.move_people();
+  world.infect_people();
+  println!("{:?}", &world);
+  world.move_people();
+  world.infect_people();
+  println!("{:?}", &world);
 }
